674141c7f0dc6386b4c78f08d873942fe12ba621
Carla Montano <cm.emprendiendo@gmail.com>
2023-10-21 21:55:07 -0500

    removed unused code

--- dashboard/models.py
+++ dashboard/models.py
@@ -51,31 +51,3 @@ class Goal(models.Model):
 
     def __str__(self):
         return self.title
-    
-    # CATEGORY_CHOICES = [
-    #     ('professional', 'Professional'),
-    #     ('personal', 'Personal'),
-    #     ('development', 'Development'),
-    #     ('spiritual', 'Spiritual'),
-    #     ('faith', 'Faith'),
-    #     ('charity', 'Charity'),
-    # ]
-
-    # TYPE_CHOICES = [
-    #     ('long_term', 'Long Term'),
-    #     ('mid_term', 'Mid Term'),
-    #     ('weekly', 'Weekly'),
-    # ]
-
-    # user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='activities')
-    # title = models.CharField(max_length=255)
-    # type = models.CharField(max_length=20, choices=TYPE_CHOICES)
-    # summary = models.TextField()
-    # description = models.TextField()
-    # action_items = models.TextField()
-    # category = models.CharField(max_length=20, choices=CATEGORY_CHOICES)
-    # created_at = models.DateTimeField(auto_now_add=True)
-    # updated_at = models.DateTimeField(auto_now=True)
-
-    # def __str__(self):
-    #     return self.title
